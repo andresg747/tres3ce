0700f4933def6f11cd292b1976537a5bef7f6542
Andres Garcia <andresg747@gmail.com>
2023-06-29 23:46:18 -0300

    Updated favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,56 +39,56 @@ export default {
       keyframes: {
         'gradient-foreground-1': {
           'from, 16.667%, to': {
-            opacity: 1,
+            opacity: '1',
           },
           '33.333%, 83.333%': {
-            opacity: 0,
+            opacity: '0',
           },
         },
         'gradient-background-1': {
           'from, 16.667%, to': {
-            opacity: 0,
+            opacity: '0',
           },
           '25%, 91.667%': {
-            opacity: 1,
+            opacity: '1',
           },
         },
         'gradient-foreground-2': {
           'from, to': {
-            opacity: 0,
+            opacity: '0',
           },
           '33.333%, 50%': {
-            opacity: 1,
+            opacity: '1',
           },
           '16.667%, 66.667%': {
-            opacity: 0,
+            opacity: '0',
           },
         },
         'gradient-background-2': {
           'from, to': {
-            opacity: 1,
+            opacity: '1',
           },
           '33.333%, 50%': {
-            opacity: 0,
+            opacity: '0',
           },
           '25%, 58.333%': {
-            opacity: 1,
+            opacity: '1',
           },
         },
         'gradient-foreground-3': {
           'from, 50%, to': {
-            opacity: 0,
+            opacity: '0',
           },
           '66.667%, 83.333%': {
-            opacity: 1,
+            opacity: '1',
           },
         },
         'gradient-background-3': {
           'from, 58.333%, 91.667%, to': {
-            opacity: 1,
+            opacity: '1',
           },
           '66.667%, 83.333%': {
-            opacity: 0,
+            opacity: '0',
           },
         },
       },
