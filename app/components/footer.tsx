a28af6e97c59e887a770b3854760528449de8f06
Andres Garcia <andresg747@gmail.com>
2023-06-29 23:30:44 -0300

    Updated tres3ce

---
+++ app/components/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="flex flex-row">
+                <div className="bg-primaryRed w-1/5 h-6"></div>
+                <div className="bg-primaryBlue w-1/5 h-6"></div>
+                <div className="bg-darkGray w-1/5 h-6"></div>
+                <div className="bg-lightGray w-1/5 h-6"></div>
+                <div className="bg-lightestGray w-1/5 h-6"></div>
+            </div>
+        </footer>
+    )
+}
---
+++ app/components/icons/email.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function EmailIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline"
+        >
+            <path
+                stroke="#404040"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 7l6.2 4.65a3 3 0 003.6 0L20 7"
+            ></path>
+            <rect
+                width="18"
+                height="14"
+                x="3"
+                y="5"
+                stroke="#404040"
+                strokeLinecap="round"
+                strokeWidth="2"
+                rx="2"
+            ></rect>
+        </svg>
+    );
+}
+
