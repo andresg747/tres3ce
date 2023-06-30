4c37afe79d1734022d172e9fb1fd312b2fb41f5e
Andres Garcia <andresg747@gmail.com>
2023-06-30 08:19:02 -0300

    Update theme

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -35,10 +35,10 @@ export default async function ContactPage() {
           style={{ resize: 'none' }}
           rows={3}
           required
-          className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
+          className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-neutral-800 text-neutral-100"
         />
         <button
-          className="flex items-center justify-center absolute right-2 top-2 px-2 py-1 font-medium h-7 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16"
+          className="flex items-center justify-center absolute right-2 top-2 px-2 py-1 font-medium h-7 bg-neutral-700 text-neutral-100 rounded w-16"
           type="submit"
         >
           Enviar
--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={clsx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        'text-white bg-[#111010]',
         graphik.variable
       )}
     >
