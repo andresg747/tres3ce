b5bdfd12bc4bcd2b7cb9c33093d9fa51eed660b6
Andres Garcia <andresg747@gmail.com>
2023-06-30 08:53:29 -0300

    Testing css

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,11 +2,11 @@ export default function Footer() {
     return (
         <footer>
             <div className="flex flex-row">
-                <div className="bg-primaryRed w-1/5 h-6"></div>
-                <div className="bg-primaryBlue w-1/5 h-6"></div>
-                <div className="bg-darkGray w-1/5 h-6"></div>
-                <div className="bg-lightGray w-1/5 h-6"></div>
-                <div className="bg-lightestGray w-1/5 h-6"></div>
+                <div className="bg-primaryRed w-1/5 h-8"></div>
+                <div className="bg-primaryBlue w-1/5 h-8"></div>
+                <div className="bg-darkGray w-1/5 h-8"></div>
+                <div className="bg-lightGray w-1/5 h-8"></div>
+                <div className="bg-lightestGray w-1/5 h-8"></div>
             </div>
         </footer>
     )
