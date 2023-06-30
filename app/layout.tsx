bde9918682e6942aceea3c7717d45d28e95911a8
Andres Garcia <andresg747@gmail.com>
2023-06-29 23:46:13 -0300

    Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,8 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
+  favicon: 
 };
 
 export default function RootLayout({
